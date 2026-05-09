bb300dca705fc9b65b8707d57b0a4dc59947e848
FLINT <flint.plumbing.heating@gmail.com>
2026-05-09 22:01:56 +0100

    Added resend key

--- app/api/quote/route.ts
+++ app/api/quote/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_KRdwdryT_DCRxTyXL8HpomG51QYJDnFzV");
 
 export async function POST(request: Request) {
   try {
